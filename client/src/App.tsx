9766612b93ede56f997dc515e0e4d3463c672289
Julia Nascimento <ju.araujo11@yahoo.com.br>
2021-11-17 11:10:06 -0300

    Create connection with websocket sending an image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
+  const [message, setMessage] = useState('')
+
+  function base64DecodeUnicode(str: string) {
+    // Convert Base64 encoded bytes to percent-encoding, and then get the original string.
+    const percentEncodedStr = atob(str)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      })
+      .join('')
+
+    return decodeURIComponent(percentEncodedStr)
+  }
+
+  const socketClient = new WebSocket('ws://localhost:8082')
+
+  socketClient.addEventListener('open', () => {
+    console.log('temos conexao!')
+
+    socketClient.send('oi, tudo bem?')
+  })
+
+  socketClient.addEventListener('message', event => {
+    // event.data.text().then((text: any) => {
+    //   console.log(text)
+    //   setMessage(text)
+    // })
+    setMessage(event.data)
+    console.log('q isso', event.data)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={message} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +47,7 @@ const App = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Socorro
+          {message}
         </a>
       </header>
     </div>
